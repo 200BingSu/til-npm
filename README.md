be245e5311033d70c46c2d182cabeb3140f4a8e6
github 200BingSu <github 200BingSu@gmail.com>
2024-12-20 10:58:12 +0900

    moment, dayjs

--- README.md
+++ README.md
@@ -1,9 +1,140 @@
-# Nivo Chart
+# 날짜를 다루는 라이브러리
 
-## 설치
+## 1. moment
 
+### 1.1.설치
+
+- https://momentjs.com/
+- `npm i moment`
+
+### 1.3. 활용예
+
+```jsx
+import moment from "moment";
+
+// 서버에서 Response 된 데이터
+const getData = [
+  {
+    id: 1,
+    title: "swaggr 완료",
+    createAt: "2024-12-13T10:00:00Z",
+  },
+  {
+    id: 2,
+    title: "react 완료",
+    createAt: "2024-12-18T10:00:00Z",
+  },
+];
+
+function App() {
+  // 오늘의 날짜
+  const todayMoment = moment().format("YYYY-MM-DD");
+  return (
+    <div>
+      <h1>moment 활용 날짜관련</h1>
+      <div>
+        <p>오늘은 {todayMoment}</p>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 날짜 :{" "}
+              {moment(item.createAt).format("YYYY-MM-DD")}
+            </p>
+          );
+        })}
+
+        <h2>moment 를 활용한 5일 뒤 날짜 계산하기 </h2>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 5일 뒤의 날짜 :{" "}
+              {moment(item.createAt).add(5, "days").format("YYYY-MM-DD")}
+            </p>
+          );
+        })}
+        <h3>moment 를 활용한 시간이 얼마나 지났는지? </h3>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 얼마나 지났는지 :{" "}
+              {moment(item.createAt).fromNow()}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export default App;
 ```
-npm i @nivo/core
+
+## 2. Dayjs
+
+### 2.1. 설치
+
+- [활용참조](https://velog.io/@hongsoom/Library-day.js-%EB%82%A0%EC%A7%9C-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC)
+
+### 2.2. 활용
+
+### 2.3. 활용 예제
+
+```jsx
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime); //relativeTime 사용하기
+
+// 서버에서 Response 된 데이터
+const getData = [
+  {
+    id: 1,
+    title: "swaggr 완료",
+    createAt: "2024-12-13T10:00:00Z",
+  },
+  {
+    id: 2,
+    title: "react 완료",
+    createAt: "2024-12-18T10:00:00Z",
+  },
+];
+
+function App() {
+  // 오늘의 날짜
+  const todayMoment = dayjs().format("YYYY-MM-DD");
+  return (
+    <div>
+      <h1>moment 활용 날짜관련</h1>
+      <div>
+        <p>오늘은 {todayMoment}</p>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 날짜 :{" "}
+              {dayjs(item.createAt).format("YYYY-MM-DD")}
+            </p>
+          );
+        })}
+
+        <h2>moment 를 활용한 5일 뒤 날짜 계산하기 </h2>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 5일 뒤의 날짜 :{" "}
+              {dayjs(item.createAt).add(5, "day").format("YYYY-MM-DD")}
+            </p>
+          );
+        })}
+        <h3>moment 를 활용한 시간이 얼마나 지났는지? </h3>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 얼마나 지났는지 :{" "}
+              {dayjs(item.createAt).fromNow()}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export default App;
 ```
-## 각 차트 모양을 보고 설치를 별도 진행
-- Line Chart `npm i @nivo/line`
--- src/App.jsx
+++ src/App.jsx
@@ -1,349 +1,58 @@
-import { ResponsiveLine } from "@nivo/line";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime); //relativeTime 사용하기
 
+// 서버에서 Response 된 데이터
 const getData = [
   {
-    id: "japan",
-    color: "hsl(347, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 48,
-      },
-      {
-        x: "helicopter",
-        y: 104,
-      },
-      {
-        x: "boat",
-        y: 242,
-      },
-      {
-        x: "train",
-        y: 26,
-      },
-      {
-        x: "subway",
-        y: 281,
-      },
-      {
-        x: "bus",
-        y: 157,
-      },
-      {
-        x: "car",
-        y: 172,
-      },
-      {
-        x: "moto",
-        y: 76,
-      },
-      {
-        x: "bicycle",
-        y: 265,
-      },
-      {
-        x: "horse",
-        y: 148,
-      },
-      {
-        x: "skateboard",
-        y: 106,
-      },
-      {
-        x: "others",
-        y: 37,
-      },
-    ],
+    id: 1,
+    title: "swaggr 완료",
+    createAt: "2024-12-13T10:00:00Z",
   },
   {
-    id: "france",
-    color: "hsl(253, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 144,
-      },
-      {
-        x: "helicopter",
-        y: 83,
-      },
-      {
-        x: "boat",
-        y: 246,
-      },
-      {
-        x: "train",
-        y: 78,
-      },
-      {
-        x: "subway",
-        y: 248,
-      },
-      {
-        x: "bus",
-        y: 222,
-      },
-      {
-        x: "car",
-        y: 299,
-      },
-      {
-        x: "moto",
-        y: 244,
-      },
-      {
-        x: "bicycle",
-        y: 89,
-      },
-      {
-        x: "horse",
-        y: 65,
-      },
-      {
-        x: "skateboard",
-        y: 44,
-      },
-      {
-        x: "others",
-        y: 30,
-      },
-    ],
-  },
-  {
-    id: "us",
-    color: "hsl(26, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 227,
-      },
-      {
-        x: "helicopter",
-        y: 8,
-      },
-      {
-        x: "boat",
-        y: 218,
-      },
-      {
-        x: "train",
-        y: 190,
-      },
-      {
-        x: "subway",
-        y: 213,
-      },
-      {
-        x: "bus",
-        y: 59,
-      },
-      {
-        x: "car",
-        y: 82,
-      },
-      {
-        x: "moto",
-        y: 286,
-      },
-      {
-        x: "bicycle",
-        y: 151,
-      },
-      {
-        x: "horse",
-        y: 22,
-      },
-      {
-        x: "skateboard",
-        y: 189,
-      },
-      {
-        x: "others",
-        y: 195,
-      },
-    ],
-  },
-  {
-    id: "germany",
-    color: "hsl(85, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 30,
-      },
-      {
-        x: "helicopter",
-        y: 187,
-      },
-      {
-        x: "boat",
-        y: 278,
-      },
-      {
-        x: "train",
-        y: 178,
-      },
-      {
-        x: "subway",
-        y: 242,
-      },
-      {
-        x: "bus",
-        y: 209,
-      },
-      {
-        x: "car",
-        y: 256,
-      },
-      {
-        x: "moto",
-        y: 91,
-      },
-      {
-        x: "bicycle",
-        y: 95,
-      },
-      {
-        x: "horse",
-        y: 189,
-      },
-      {
-        x: "skateboard",
-        y: 270,
-      },
-      {
-        x: "others",
-        y: 34,
-      },
-    ],
-  },
-  {
-    id: "norway",
-    color: "hsl(18, 70%, 50%)",
-    data: [
-      {
-        x: "plane",
-        y: 162,
-      },
-      {
-        x: "helicopter",
-        y: 82,
-      },
-      {
-        x: "boat",
-        y: 142,
-      },
-      {
-        x: "train",
-        y: 171,
-      },
-      {
-        x: "subway",
-        y: 45,
-      },
-      {
-        x: "bus",
-        y: 142,
-      },
-      {
-        x: "car",
-        y: 142,
-      },
-      {
-        x: "moto",
-        y: 239,
-      },
-      {
-        x: "bicycle",
-        y: 167,
-      },
-      {
-        x: "horse",
-        y: 86,
-      },
-      {
-        x: "skateboard",
-        y: 296,
-      },
-      {
-        x: "others",
-        y: 147,
-      },
-    ],
+    id: 2,
+    title: "react 완료",
+    createAt: "2024-12-18T10:00:00Z",
   },
 ];
 
-const App = () => {
+function App() {
+  // 오늘의 날짜
+  const todayMoment = dayjs().format("YYYY-MM-DD");
   return (
-    <div style={{ width: "80%", height: 500, margin: "0 auto" }}>
-      <ResponsiveLine
-        data={getData}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: "point" }}
-        yScale={{
-          type: "linear",
-          min: "auto",
-          max: "auto",
-          stacked: true,
-          reverse: false,
-        }}
-        yFormat=" >-.2f"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "transportation",
-          legendOffset: 36,
-          legendPosition: "middle",
-          truncateTickAt: 0,
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "count",
-          legendOffset: -40,
-          legendPosition: "middle",
-          truncateTickAt: 0,
-        }}
-        pointSize={10}
-        pointColor={{ theme: "background" }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: "serieColor" }}
-        pointLabel="data.yFormatted"
-        pointLabelYOffset={-12}
-        enableTouchCrosshair={true}
-        useMesh={true}
-        legends={[
-          {
-            anchor: "bottom-right",
-            direction: "column",
-            justify: false,
-            translateX: 100,
-            translateY: 0,
-            itemsSpacing: 0,
-            itemDirection: "left-to-right",
-            itemWidth: 80,
-            itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: "circle",
-            symbolBorderColor: "rgba(0, 0, 0, .5)",
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemBackground: "rgba(0, 0, 0, .03)",
-                  itemOpacity: 1,
-                },
-              },
-            ],
-          },
-        ]}
-      />
+    <div>
+      <h1>moment 활용 날짜관련</h1>
+      <div>
+        <p>오늘은 {todayMoment}</p>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 날짜 :{" "}
+              {dayjs(item.createAt).format("YYYY-MM-DD")}
+            </p>
+          );
+        })}
+
+        <h2>moment 를 활용한 5일 뒤 날짜 계산하기 </h2>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 5일 뒤의 날짜 :{" "}
+              {dayjs(item.createAt).add(5, "day").format("YYYY-MM-DD")}
+            </p>
+          );
+        })}
+        <h3>moment 를 활용한 시간이 얼마나 지났는지? </h3>
+        {getData.map(item => {
+          return (
+            <p key={item.id}>
+              아이디 : {item.id} 제목 : {item.title} 얼마나 지났는지 :{" "}
+              {dayjs(item.createAt).fromNow()}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
-};
+}
 export default App;
