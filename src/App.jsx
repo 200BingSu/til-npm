51db4ba35d0c5f3c9b9911ca1400f1e3b6de7cb1
github 200BingSu <github 200BingSu@gmail.com>
2025-01-20 10:06:31 +0900

    [docs]antD

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { ConfigProvider } from "antd";
 import JoinForm from "./components/JoinForm";
+import { useState } from "react";
+
+const ThemeData = {
+  token: {
+    colorPrimary: "#0DD1FD",
+    colorSecondary1: "#FFF600", // Secondary 1
+    colorSecondary2: "#6B4AD6", // Secondary 2
+    colorSecondary3: "#FB653D", // Secondary 3
+  },
+  Button: {
+    colorPrimary: "#02AED5",
+    colorSecondary1: "#CCC500", // Secondary 1
+    colorSecondary2: "#4F2CC1", // Secondary 2
+    colorSecondary3: "#FA3D0B", // Secondary 3
+  },
+};
 
 function App() {
+  const [data, setData] = useState(ThemeData);
   return (
-    <div>
-      <JoinForm />
-    </div>
+    <ConfigProvider theme={data}>
+      <JoinForm theme={data} />
+    </ConfigProvider>
   );
 }
 export default App;
--- src/components/JoinForm.jsx
+++ src/components/JoinForm.jsx
@@ -1,25 +1,32 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Radio } from "antd";
+import { useState } from "react";
 
-const JoinForm = () => {
-  // 1. 기본 값 넣기(default)
+const JoinForm = ({ theme }) => {
+  // theme을 props로 받음
+  console.log(theme); // theme이 정상적으로 출력됨
+
+  const [value, setValue] = useState(1);
+
   const initialValues = {
     userid: "",
     userpass: "hello",
     nickname: "길동",
     email: "a@a.net",
   };
-  // 2. 라벨 넣기
-  // 3. placeholder 넣기
-  // 4. 필수값 표현하기
-  // 5. 필수값 안내 메세지 표시하기
-  // 6. 각 필드의 입력 중인 값 알아내기
-  // 7. 확인 버튼 시 최종 입력값
+
   const onChangeField = _field => {
     console.log(_field[0].value);
   };
+
   const onFinished = values => {
     console.log(values);
   };
+
+  const onChange = e => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
+  };
+
   return (
     <div>
       <Form
@@ -30,6 +37,7 @@ const JoinForm = () => {
         }
         onFinish={values => onFinished(values)}
       >
+        {/* Form Items */}
         <Form.Item
           name={"userid"}
           label="아이디"
@@ -57,27 +65,34 @@ const JoinForm = () => {
         >
           <Input.Password placeholder="비밀번호를 입력하세요" />
         </Form.Item>
-        <Form.Item name={"nickname"} label="닉네임">
-          <Input placeholder="닉네임을 입력하세요" />
-        </Form.Item>
-        <Form.Item
-          name={"email"}
-          label="이메일"
-          required={true}
-          rules={[
-            { required: true, message: "이메일은 필수사항입니다." },
-            { type: "email", message: "이메일 형식에 맞지 않습니다." },
-            { min: 4, message: "이메일은 4자 이상입니다." },
-            { max: 8, message: "이메일은 8자 이하입니다." },
-          ]}
+        {/* Buttons */}
+        <Button
+          style={{
+            backgroundColor: theme.token.colorSecondary1,
+            color: "#fff",
+          }}
         >
-          <Input placeholder="이메일을 입력하세요" />
-        </Form.Item>
-        <Form.Item>
-          <Button htmlType="submit">확인</Button>
-        </Form.Item>
+          Secondary 1
+        </Button>
+        <Button
+          style={{
+            backgroundColor: theme.token.colorSecondary2,
+            color: "#fff",
+          }}
+        >
+          Secondary 2
+        </Button>
+        <Button
+          style={{
+            backgroundColor: theme.token.colorSecondary3,
+            color: "#fff",
+          }}
+        >
+          Secondary 3
+        </Button>
       </Form>
     </div>
   );
 };
+
 export default JoinForm;
