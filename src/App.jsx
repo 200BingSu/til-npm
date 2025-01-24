7e9047b9f4db393018ec28912ef2786c3da72966
github 200BingSu <github 200BingSu@gmail.com>
2025-01-24 11:43:03 +0900

    [docs]axios-file

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,30 @@
 import axios from "axios";
-import { useRecoilState } from "recoil";
-import { loginInfoState } from "./atoms/userInfo";
-import { removeCookie, setCookie } from "./utils/cookie";
-import { Button } from "antd";
 
-function App() {
-  const [loginInfo, setLoginInfo] = useRecoilState(loginInfoState);
-  const login = async () => {
+const App = () => {
+  const handleSubmit = async () => {
     try {
-      const res = await axios.post("/api/user/sign-in", {
-        email: "dkssud123@tmails.net",
-        upw: "1q2w3e4R!",
-      });
-      console.log(res.data);
-      // 리코일에 전체 저장
-      setLoginInfo(res.data.resultData);
-      // 쿠키에 보관하기
-      setCookie("accessToken", res.data.resultData.accessToken);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div>
-      <h1>JWT : accessToken 만 존재</h1>
-    </div>
-  );
-}
-export default App;
+      const formData = new FormData();
+      // 보내야 하는 데이터
+      const sendData = {
+        email: "1234park@naver.com",
+        upw: "1111",
+        name: "홍길동",
+        phone: "01012345678",
+      };
+      formData.append(
+        "p",
+        new Blob([JSON.stringify(sendData)], { type: "application/json" }),
+      );
+      if (파일) {
+        formData.append("pic", 파일);
+      }
 
-function Test() {
-  const [loginInfo, setLoginInfo] = useRecoilState(loginInfoState);
-  const accessToken = loginInfo.accessToken;
-  const callFn = async () => {
-    try {
-      const res = await axios.get("/api/user", {
+      const res = await axios.post("/api/user/sign-up", formData, {
         headers: {
-          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "multipart/form-data",
         },
       });
       console.log(res.data);
-      // 만약에 인증키 만료라면(unAthorized)
-      if (res.status === 401) {
-        // 인증키가 만료되었다고 온다면
-        // 선택을 해야한다.
-        removeCookie("accessToken");
-        // 1. 강제로 다시 로그인
-        // setLoginInfo({});
-        // alert("다시 로그인을 해주세요");
-        // alert("라우터로 login창으로 이동합니다.");
-        // 2. 다시 accessToken 재발행
-        resetToken();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const resetToken = async () => {
-    try {
-      const res = await axios.get(`/api/user/access-token`);
-      console.log(res.data);
-      setCookie("accessToken", res.data.resultData.accessToken);
-      setLoginInfo(prev => ({
-        ...prev,
-        accessToken: res.data.ressulData.accessToken,
-      }));
-      // 본래 하려던 api 호출
-      callFn();
     } catch (error) {
       console.log(error);
     }
@@ -74,10 +32,9 @@ function Test() {
 
   return (
     <div>
-      <h1>테스트</h1>
-      <Button type="primary" onClick={() => callFn()}>
-        api 호출
-      </Button>
+      <h1>File 및 json 데이터 post 테스트</h1>
+      <button onClick={() => handleSubmit()}>업로드</button>
     </div>
   );
-}
+};
+export default App;
